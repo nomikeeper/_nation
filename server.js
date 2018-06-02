6fd8adf62cd9a25ea421c6662663897047d0a906
Nomi G <nomikeeper@gmail.com>
2018-06-02 16:11:55 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+// Creating the app
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+
+// Maintenance page
+/*
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+*/
+// Help static page
+app.use(express.static(__dirname+ '/public'))
+
+// Current Year getter
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text)=> {
+    return text.toUpperCase();
+});
+
+// Home page
+app.get('/', (req, res)=> { 
+   res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to Digital nation'
+   });
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+app.listen(3000);
