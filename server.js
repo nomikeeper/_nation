763a361e51828a3dea814c60de3538bb6053a05f
Nomi G <nomikeeper@gmail.com>
2018-06-02 16:24:58 +0900

    Port config added to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 // Creating the app
 var app = express();
 
@@ -52,4 +52,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
